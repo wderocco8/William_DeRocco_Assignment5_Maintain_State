6e2f6bb14393b227739b47aab326d7a5efc5fbb5
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-03-12 22:53:16 -0400

    added template...

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "William_DeRocco_Assignment5_Maintain_State"
+include(":app")
+ 
